21ce92629442d2f3be9b2baa0d2114f1aa4b8d4e
Rich Morgan <ionux@users.noreply.github.com>
2015-01-13 15:51:39 -0500

    Fix for missing co-prime check
    And added documentation for all functions.

--- src/Bitpay/Math/BcEngine.php
+++ src/Bitpay/Math/BcEngine.php
@@ -6,87 +6,99 @@
 
 namespace Bitpay\Math;
 
+/**
+ * Engine class for the Binary Calculator Math extension.
+ *
+ * @package Bitpay
+ */
 class BcEngine implements EngineInterface
 {
     const HEX_CHARS = '0123456789abcdef';
+
     /**
-     * @param String $a Numeric String
-     * @param String $b Numeric String
+     * Public constructor method where we're setting the scale
+     * for all Binary Calculator math operations to zero. We
+     * do not want nor care about any floats in our math lib.
      */
     public function __construct()
     {
-        bcscale(0);
+        if (false === bscale(0)) {
+            throw new \Exception('[ERROR] In BcEngine::_construct(): Could not set the scale for BC math operations.');
+        }
     }
 
     /**
-     * @param String $a Numeric String
-     * @param String $b Numeric String
+     * Adds two arbitrary precision integers, BC style.
+     *
+     * @param  string $a  First number in decimal or hex format.
+     * @param  string $b  Second number in decimal or hex format.
+     * @return string     The result of the operation.
      */
     public function add($a, $b)
     {
-        $a = $this->input($a);
-        $b = $this->input($b);
-
-        return bcadd($a, $b);
+        return bcadd($this->input($a), $this->input($b));
     }
 
     /**
-     * @param String $a Numeric String
-     * @param String $b Numeric String
+     * Compares two arbitrary precision integers, BC style.
+     * Returns 0 if the two operands are equal, 1 if $a is
+     * larger than $b, or -1 if $b is larger than $a.
+     *
+     * @param  string $a  First number in decimal or hex format.
+     * @param  string $b  Second number in decimal or hex format.
+     * @return string     The result of the operation.
      */
     public function cmp($a, $b)
     {
-        $a = $this->input($a);
-        $b = $this->input($b);
-
-        return bccomp($a, $b);
+        return bccomp($this->input($a), $this->input($b));
     }
 
     /**
-     * @param String $a Numeric String
-     * @param String $b Numeric String
+     * Divides two arbitrary precision integers, BC style.
+     * Returns $a / $b if $b > 0, null otherwise.
+     *
+     * @param  string $a  First number in decimal or hex format.
+     * @param  string $b  Second number in decimal or hex format.
+     * @return string     The result of the operation.
      */
     public function div($a, $b)
     {
-        $a = $this->input($a);
-        $b = $this->input($b);
-
-        return bcdiv($a, $b);
+        return bcdiv($this->input($a), $this->input($b));
     }
 
     /**
      * Finds inverse number $inv for $num by modulus $mod, such as:
      *     $inv * $num = 1 (mod $mod)
      *
-     * @param string $num
-     * @param string $mod
-     * @return string
-     * @access public
+     * @param  string $num  First number in decimal or hex format.
+     * @param  string $mod  Second number in decimal or hex format.
+     * @return string       The result of the operation.
      */
     public function invertm($num, $mod)
     {
         $num = $this->input($num);
         $mod = $this->input($mod);
 
+        if (false === $this->coprime($num, $mod)) {
+        	return null;
+        }
+
         $x = '1';
         $y = '0';
+
         $num1 = $mod;
 
         do {
             $tmp = bcmod($num, $num1);
+            $q   = bcdiv($num, $num1);
 
-            $q = bcdiv($num, $num1);
-
-            $num = $num1;
-
+            $num  = $num1;
             $num1 = $tmp;
 
             $tmp = bcsub($x, bcmul($y, $q));
 
             $x = $y;
-
             $y = $tmp;
-
         } while (bccomp($num1, '0'));
 
         if (bccomp($x, '0') < 0) {
@@ -101,8 +113,12 @@ class BcEngine implements EngineInterface
     }
 
     /**
-     * @param String $a Numeric String
-     * @param String $b Numeric String
+     * Finds the modulus of $a mod $b, BC style.
+     * Returns null if $b == 0.
+     *
+     * @param  string $a  First number in decimal or hex format.
+     * @param  string $b  Second number in decimal or hex format.
+     * @return string     The result of the operation.
      */
     public function mod($a, $b)
     {
@@ -117,54 +133,71 @@ class BcEngine implements EngineInterface
     }
 
     /**
-     * @param String $a Numeric String
-     * @param String $b Numeric String
+     * Multiplies two arbitrary precision integers, BC style.
+     *
+     * @param  string $a  First number in decimal or hex format.
+     * @param  string $b  Second number in decimal or hex format.
+     * @return string     The result of the operation.
      */
     public function mul($a, $b)
     {
-        $a = $this->input($a);
-        $b = $this->input($b);
-
-        return bcmul($a, $b);
+        return bcmul($this->input($a), $this->input($b));
     }
 
     /**
-     * @param String $a Numeric String
-     * @param String $b Numeric String
+     * Raises an arbitrary precision integer $a to the power of $b, BC style.
+     *
+     * @param  string $a  First number in decimal or hex format.
+     * @param  string $b  Second number in decimal or hex format.
+     * @return string     The result of the operation.
      */
     public function pow($a, $b)
     {
-        $a = $this->input($a);
-        $b = $this->input($b);
-
-        return bcpow($a, $b);
+        return bcpow($this->input($a), $this->input($b));
     }
 
     /**
-     * @param String $a Numeric String
-     * @param String $b Numeric String
+     * Subtracts two arbitrary precision integers, BC style.
+     *
+     * @param  string $a  First number in decimal or hex format.
+     * @param  string $b  Second number in decimal or hex format.
+     * @return string     The result of the operation.
      */
     public function sub($a, $b)
     {
-        $a = $this->input($a);
-        $b = $this->input($b);
-
-        return bcsub($a, $b);
+        return bcsub($this->input($a), $this->input($b));
     }
 
+    /**
+     * Utility function for checking, cleaning and converting
+     * any non-decimal string formatted numbers to decimal.
+     *
+     * @param  string $x  The value to sanitize.
+     * @return string     Returns either '0' or the sanitized value.
+     */
     public function input($x)
     {
-        if (empty($x)) {
+        $sign = '';
+        $hex  = '';
+        $curr = '';
+        $dec  = '';
+        $hlen = 0;
+
+        if (false === isset($x) || true === empty($x)) {
             return '0';
         }
+
         $x = strtolower(trim($x));
+
         if (preg_match('/^(-?)0x([0-9a-f]+)$/', $x, $matches)) {
             $sign = $matches[1];
-            $hex = $matches[2];
+            $hex  = $matches[2];
 
-            for ($dec = '0', $i = 0; $i < strlen($hex); $i++) {
-                $current = strpos('0123456789abcdef', $hex[$i]);
-                $dec     = bcadd(bcmul($dec, 16), $current);
+            $hlen = strlen($hex);
+
+            for ($dec = '0', $i = 0; $i < $hlen; $i++) {
+                $current = strpos(self::HEX_CHARS, $hex[$i]);
+                $dec     = bcadd(bcmul($dec, '16'), $current);
             }
 
             return $sign.$dec;
@@ -172,7 +205,7 @@ class BcEngine implements EngineInterface
         } elseif (preg_match('/^-?[0-9]+$/', $x)) {
             return $x;
         } else {
-            throw new \Exception("The input must be a numeric string in decimal or hexadecimal (with leading 0x) format.\n".var_export($x, true));
+            throw new \Exception('[ERROR] In BcEngine::input(): The parameter must be a numeric string in decimal or hexadecimal (with leading 0x) format.  Parameter given: ' . "\n" . var_export($x, true));
         }
 
     }
@@ -181,30 +214,39 @@ class BcEngine implements EngineInterface
      * Function to determine if two numbers are
      * co-prime according to the Euclidean algo.
      *
-     * @param  string $a First param to check.
-     * @param  string $b Second param to check.
-     * @return bool   Whether the params are cp.
+     * @param  string $a  First param to check.
+     * @param  string $b  Second param to check.
+     * @return bool       The result of the operation.
      */
     public function coprime($a, $b)
     {
         $small = 0;
         $diff  = 0;
+
         while (bccomp($a, '0') > 0 && bccomp($b, '0') > 0) {
-            if (bccomp($a, $b) == -1) {
-                $small = $a;
-                $diff  = bcmod($b, $a);
+        	$comp_result = bccomp($a, $b);
+
+        	switch ($comp_result) {
+        		case -1:
+                    $small = $a;
+                    $diff  = bcmod($b, $a);
+                    break;
+        		case 1:
+                    $small = $b;
+                    $diff  = bcmod($a, $b);
+                    break;
+        		case 0:
+                    $small = $a;
+                    $diff  = bcmod($b, $a);
+                    break;
+        		default:
+        			throw new \Exception('[ERROR] In BcEngine::coprime(): Comparison of the two parameters produced an invalid result.');
             }
-            if (bccomp($a, $b) == 1) {
-                $small = $b;
-                $diff = bcmod($a, $b);
-            }
-            if (bccomp($a, $b) == 0) {
-                $small = $a;
-                $diff  = bcmod($b, $a);
-            }
+
             $a = $small;
             $b = $diff;
         }
+
         if (bccomp($a, '1') == 0) {
             return true;
         }
