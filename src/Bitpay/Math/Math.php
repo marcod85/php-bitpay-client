5947a2aae5c6803cb9bb1284785fda6152a8e749
Rich Morgan <ionux@users.noreply.github.com>
2015-01-13 14:29:56 -0500

    Added extra error handling and parameter checking
    Also added some class documentation.

--- src/Bitpay/Math/Math.php
+++ src/Bitpay/Math/Math.php
@@ -6,14 +6,21 @@
 
 namespace Bitpay\Math;
 
+/**
+ * Math interface class which handles the routing to the correct engine,
+ * either GMP or BC, for all math operations with the former preferred.
+ */
 class Math
 {
-    private static $engine;
+    private static $engine = null;
 
     public static function setEngine($engine)
     {
-        static::$engine = $engine;
+        if (true === isset($engine)) {
+            static::$engine = $engine;
+        }
     }
+
     public static function getEngine()
     {
         return static::$engine;
@@ -21,16 +28,28 @@ class Math
 
     public static function __callStatic($name, $arguments)
     {
-        if (is_null(static::$engine)) {
-            if (extension_loaded('gmp')) {
+        if (false === isset(static::$engine) || true === empty(static::$engine)) {
+            if (true === extension_loaded('gmp')) {
                 static::$engine = new GmpEngine();
-            } elseif (extension_loaded('bcmath')) {
+            } else if (true === extension_loaded('bcmath')) {
                 static::$engine = new BcEngine();
             } else {
-                throw new \Exception('The GMP or BCMATH extension for PHP is required.');
+                throw new \Exception('[ERROR] In Math::__callStatic(): The GMP or BCMATH extension for PHP is required but neither are found on this system.');
             }
         }
 
-        return call_user_func_array(array(static::$engine, $name), $arguments);
+        /*
+         * Check to ensure our math function name and the 
+         * requisite parameters were passed to this function.
+         */
+        if (true  === isset($name) && true  === isset($arguments) &&
+            false === empty($name) && false === empty($arguments)) {
+
+            return call_user_func_array(array(static::$engine, $name), $arguments);
+
+        } else {
+            throw new \Exception('[ERROR] In Math::__callStatic(): Missing or invalid $name or $arguments parameters.');
+        }
+
     }
 }
