e892b631510ad742adc67399b4edc5e6a1d39c5e
Rich Morgan <ionux@users.noreply.github.com>
2015-01-13 18:52:08 -0500

    Added more documentation
    Added parameter checking function

--- src/Bitpay/Math/GmpEngine.php
+++ src/Bitpay/Math/GmpEngine.php
@@ -6,77 +6,142 @@
 
 namespace Bitpay\Math;
 
+/**
+ * Engine class for the GNU Multiple Precision Math extension.
+ *
+ * @package Bitpay
+ */
 class GmpEngine implements EngineInterface
 {
     /**
-     * @param String $a Numeric String
-     * @param String $b Numeric String
+     * Adds two arbitrary precision integers.
+     *
+     * @param  string $a  First number in decimal or hex format.
+     * @param  string $b  Second number in decimal or hex format.
+     * @return string     The result of the operation.
      */
     public function add($a, $b)
     {
-        return gmp_strval(gmp_add($a, $b));
+        return gmp_strval(gmp_add($this->input($a), $this->input($b)));
     }
 
     /**
-     * @param String $a Numeric String
-     * @param String $b Numeric String
+     * Compares two arbitrary precision integers. Returns a positive
+     * value if $a > $b, zero if $a = $b and a negative value if $a < $b.
+     *
+     * @param  string $a  First number in decimal or hex format.
+     * @param  string $b  Second number in decimal or hex format.
+     * @return string     The result of the operation.
      */
     public function cmp($a, $b)
     {
-        return gmp_strval(gmp_cmp($a, $b));
+        return gmp_strval(gmp_cmp($this->input($a), $this->input($b)));
     }
 
     /**
-     * @param String $a Numeric String
-     * @param String $b Numeric String
+     * Divides two arbitrary precision integers. Returns $a / $b
+     * if $b > 0, '0' otherwise. NOTE: Only throws a warning if
+     * $b == 0, not an error!
+     *
+     * @param  string $a  First number in decimal or hex format.
+     * @param  string $b  Second number in decimal or hex format.
+     * @return string     The result of the operation.
      */
     public function div($a, $b)
     {
-        return gmp_strval(gmp_div($a, $b));
+        return gmp_strval(gmp_div($this->input($a), $this->input($b)));
     }
 
     /**
-     * @param String $a Numeric String
-     * @param String $b Numeric String
+     * Finds inverse number $inv for $num by modulus $mod, such as:
+     *     $inv * $num = 1 (mod $mod)
+     *
+     * @param  string $a  First number in decimal or hex format.
+     * @param  string $b  Second number in decimal or hex format.
+     * @return string     The result of the operation.
      */
     public function invertm($a, $b)
     {
-        return gmp_strval(gmp_invert($a, $b));
+        return gmp_strval(gmp_invert($this->input($a), $this->input($b)));
     }
 
     /**
-     * @param String $a Numeric String
-     * @param String $b Numeric String
+     * Finds the modulus of $a mod $b. NOTE: Only throws a warning if
+     * $b == 0, not an error!
+     *
+     * @param  string $a  First number in decimal or hex format.
+     * @param  string $b  Second number in decimal or hex format.
+     * @return string     The result of the operation.
      */
     public function mod($a, $b)
     {
-        return gmp_strval(gmp_mod($a, $b));
+        return gmp_strval(gmp_mod($this->input($a), $this->input($b)));
     }
 
     /**
-     * @param String $a Numeric String
-     * @param String $b Numeric String
+     * Multiplies two arbitrary precision integers.
+     *
+     * @param  string $a  First number in decimal or hex format.
+     * @param  string $b  Second number in decimal or hex format.
+     * @return string     The result of the operation.
      */
     public function mul($a, $b)
     {
-        return gmp_strval(gmp_mul($a, $b));
+        return gmp_strval(gmp_mul($this->input($a), $this->input($b)));
     }
 
     /**
-     * @param String $a Numeric String
-     * @param String $b Numeric String
+     * Raises an arbitrary precision integer $a to the power of $b.
+     *
+     * @param  string $a  First number in decimal or hex format.
+     * @param  string $b  Second number in decimal or hex format.
+     * @return string     The result of the operation.
      */
     public function pow($a, $b)
     {
-        return gmp_strval(gmp_pow($a, $b));
+        return gmp_strval(gmp_pow($this->input($a), $this->input($b)));
     }
 
     /**
-     * @param String $a Numeric String
-     * @param String $b Numeric String
+     * Subtracts two arbitrary precision integers.
+     *
+     * @param  string $a  First number in decimal or hex format.
+     * @param  string $b  Second number in decimal or hex format.
+     * @return string     The result of the operation.
      */
     public function sub($a, $b)
     {
-        return gmp_strval(gmp_sub($a, $b));
+        return gmp_strval(gmp_sub($this->input($a), $this->input($b)));
+    }
+
+    /**
+     * Utility function for checking, cleaning and converting
+     * any non-hex string formatted numbers to hexadecimal.
+     *
+     * @param  string $x  The value to sanitize.
+     * @return string     Returns either '0x0' or the sanitized value.
+     */
+    public function input($x)
+    {
+        if (false === isset($x) || true === empty($x)) {
+            return '0x0';
+        }
+
+        $x = strtolower(trim($x));
+
+        if (false === ctype_xdigit($x) && true === ctype_digit($x)) {
+            // decimal number
+            return $x;
+        } else if (substr($x, 0, 2) != '0x' && true === ctype_xdigit($x)) {
+            // hex without '0x'
+            return '0x' . $x;
+        } else if (substr($x, 0, 2) == '0x' && true === ctype_xdigit(substr($x, 2))) {
+            // correctly formatted hex
+            return $x;
+        } else {
+            // bad value
+            throw new \Exception('[ERROR] In GmpEngine::input(): The parameter must be a numeric string in hexadecimal format.  Parameter given: ' . "\n" . var_export($x, true));
+        }
+
     }
 }
